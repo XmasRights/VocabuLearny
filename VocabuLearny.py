3b49b2bdab3ef9a48f0f4d90751ceadcc0feba49
XmasRights <chris.fonseka@googlemail.com>
2014-12-28 23:54:29 +0000

    Input cvs file parsed, and put into list of list data structure

--- VocabuLearny.py
+++ VocabuLearny.py
@@ -1,4 +1,35 @@
-import sys
+import sys, getopt
+import csv
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+def main(argv):
+	inputfile = ''
+	outputfile = ''
+
+	try:
+		opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+	except getopt.GetoptError:
+		print "usage: VocabuLearny.py -i <input file> -o <outputfile>\n"
+		sys.exit(2)
+
+	for opt, arg in opts:
+		if opt == '-h':
+			print "usage: VocabuLearny.py -i <input file> -o <outputfile>\n"
+			sys.exit(2)
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+
+	data = parseInputFile(inputfile)
+
+def parseInputFile(inFile):
+	data = []
+	with open(inFile, 'rb') as csvfile:
+		fileReader = csv.reader(csvfile, delimiter=",", quotechar='"')
+		for row in fileReader:
+			data.append(row)
+	return data
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
